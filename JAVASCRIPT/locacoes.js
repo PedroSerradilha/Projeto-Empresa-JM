09ea23b0a06e158a77fb62f935fd19bfb8a2f02b
Pedro Serradilha <143278378+PedroSerradilha@users.noreply.github.com>
2025-01-22 01:28:47 -0300

    Delete JAVASCRIPT directory

--- JAVASCRIPT/locacoes.js
+++
@@ -1,4 +0,0 @@
-function exibirMensagemSucesso(event) {
-    event.preventDefault(); // Previne o envio real do formulário
-    alert("Solicitação de locação enviada com sucesso!");
-}
--- JAVASCRIPT/suporte.js
+++
@@ -1,5 +0,0 @@
-function exibirMensagemSucesso(event) {
-    event.preventDefault(); // Previne o envio real do formulário (pode ser removido se você quiser enviar para um servidor)
-    alert("Enviado com sucesso!");
-  }
-  
